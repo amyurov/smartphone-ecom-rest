ac35d73437c7a31387257f1bc9cb3e0312b5bb82
Artem Yurov <amyurov.dev@mail.ru>
2024-05-24 20:55:46 +0300

    add validation annotation

--- src/main/java/com/amyurov/smartphoneecom/dto/SmartphoneEditDto.java
+++ src/main/java/com/amyurov/smartphoneecom/dto/SmartphoneEditDto.java
@@ -1,14 +1,15 @@
 package com.amyurov.smartphoneecom.dto;
 
 import com.amyurov.smartphoneecom.entity.enums.Manufacturer;
+import com.amyurov.smartphoneecom.validation.NotBlankOrNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record SmartphoneEditDto(
         Manufacturer manufacturer,
-        String model,
-        String color,
+        @NotBlankOrNull String model,
+        @NotBlankOrNull String color,
         @Positive Integer memorySize,
         @Positive BigDecimal price) {
 }
