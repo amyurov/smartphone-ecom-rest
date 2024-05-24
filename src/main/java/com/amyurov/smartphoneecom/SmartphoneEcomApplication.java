6abc4a0969a44db7ced66cde0a2c96130038d495
Artem Yurov <amyurov.dev@mail.ru>
2024-05-24 19:15:14 +0300

    first commit

---
+++ src/main/java/com/amyurov/smartphoneecom/SmartphoneEcomApplication.java
@@ -0,0 +1,13 @@
+package com.amyurov.smartphoneecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartphoneEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartphoneEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amyurov/smartphoneecom/SmartphoneEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyurov.smartphoneecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartphoneEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
