910b959646236ff9cb14e869b9d4f7f1a58f739f
Artem Yurov <amyurov.dev@mail.ru>
2024-05-24 19:52:29 +0300

    add entity smartphone.java

---
+++ src/main/java/com/amyurov/smartphoneecom/entity/Smartphone.java
@@ -0,0 +1,30 @@
+package com.amyurov.smartphoneecom.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Smartphone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Manufacturer manufacturer;
+
+    private String model;
+
+    private String color;
+
+    @Column(name = "memory_size")
+    private Integer memorySize;
+
+    private BigDecimal price;
+}
