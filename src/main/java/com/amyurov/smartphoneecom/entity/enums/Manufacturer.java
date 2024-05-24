c6385c09c000b65ed4462cc2ad4f96f6582d7dcb
Artem Yurov <amyurov.dev@mail.ru>
2024-05-24 19:55:35 +0300

    add manufacturer enum.

--- src/main/java/com/amyurov/smartphoneecom/entity/enums/Manufacturer.java
+++ src/main/java/com/amyurov/smartphoneecom/entity/enums/Manufacturer.java
@@ -1,6 +1,15 @@
 package com.amyurov.smartphoneecom.entity.enums;
-    public enum Manufacturer {
-        APPLE,
-        XIAOMI,
-        SAMSUNG
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Manufacturer {
+    APPLE,
+    XIAOMI,
+    SAMSUNG;
+
+    @JsonValue
+    public String getValue() {
+        String lowerCase = this.name().toLowerCase();
+        return lowerCase.substring(0, 1).toUpperCase() + lowerCase.substring(1);
     }
+}
