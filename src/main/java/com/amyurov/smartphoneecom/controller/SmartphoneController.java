f2492009bce5f988e4d7c524b4bf01de27b08785
Artem Yurov <amyurov.dev@mail.ru>
2024-05-25 18:59:33 +0300

    add controller JavaDoc

--- src/main/java/com/amyurov/smartphoneecom/controller/SmartphoneController.java
+++ src/main/java/com/amyurov/smartphoneecom/controller/SmartphoneController.java
@@ -19,7 +19,11 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
 import java.util.List;
-
+/**
+ * Класс {@code SmartphoneController} предоставляет RESTful эндпоинты для обработки CRUD
+ * операций над смартфонами.
+ * Использует {@link SmartphoneService} для выполнения бизнес-логики.
+ */
 @RestController
 @RequestMapping("/api/v1/smartphones")
 @RequiredArgsConstructor
@@ -29,12 +33,22 @@ public class SmartphoneController {
     private static final String BASE_URI = "api/v1/smartphones/";
     private final SmartphoneService smartphoneService;
 
+    /**
+     * Получает список всех смартфонов.
+     * @return {@link ContentResponse} - обертка содержащая список {@link SmartphoneReadDto}.
+     */
     @GetMapping
     public ResponseEntity<?> findAll() {
         var smartphoneList = smartphoneService.findAll();
         return ResponseEntity.ok(new ContentResponse(smartphoneList));
     }
 
+    /**
+     * Получает список всех смартфонов с пагинацией.
+     *
+     * @param pageable Содержит информацию о пагинации.
+     * @return {@link PaginatedContentResponse} обертка содержащая пагинированый список {@link SmartphoneReadDto}.
+     */
     @GetMapping(params = {"size"})
     public ResponseEntity<?> findAllPaginated(Pageable pageable) {
         Page<SmartphoneReadDto> allPaginated = smartphoneService.findAllPaginated(pageable);
@@ -42,6 +56,14 @@ public class SmartphoneController {
         return ResponseEntity.ok(PaginatedContentResponse.of(allPaginated));
     }
 
+    /**
+     * Получает смартфон по его ID.
+     *
+     * @param id ID смартфона, который нужно получить.
+     * @return {@link ContentResponse} - обертка содержащая запрашиваемый {@link SmartphoneReadDto}, или
+     * 404 (Not Found) если смартфон не найден.
+     * @throws ResponseStatusException если смартфон не найден.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable Integer id) {
         var readDto = smartphoneService.findById(id)
@@ -50,6 +72,13 @@ public class SmartphoneController {
         return ResponseEntity.ok(new ContentResponse(List.of(readDto)));
     }
 
+    /**
+     * Создает новый смартфон.
+     *
+     * @param smartphone Данные создаваемого смартфона.
+     * @return ResponseEntity, содержащий сохраненный смартфон.
+     * @throws ResponseStatusException при ошибке сохранения.
+     */
     @PostMapping
     public ResponseEntity<?> create(@RequestBody @Validated SmartphoneCreateDto smartphone) {
         var smartphoneReadDto = smartphoneService.create(smartphone)
@@ -59,6 +88,14 @@ public class SmartphoneController {
         return ResponseEntity.created(createdUri).body(smartphoneReadDto);
     }
 
+    /**
+     * Обновляет существующий смартфон.
+     *
+     * @param id ID обновляемого смартфона.
+     * @param smartphone Обновленные данные смартфона.
+     * @return ResponseEntity, указывающий на статус обновления.
+     * @throws ResponseStatusException если обновление не удалось.
+     */
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> update (@PathVariable Integer id, @RequestBody @Validated SmartphoneEditDto smartphone) {
         smartphoneService.update(id, smartphone)
@@ -66,10 +103,14 @@ public class SmartphoneController {
 
         return ResponseEntity.ok().build();
     }
-
+    /**
+     * Удаляет смартфон по его ID.
+     *
+     * @param id ID удаляемого смартфона.
+     * @return ResponseEntity 204 (No Content) если удаление успешно, или 404 (Not Found) если смартфон не найден.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
         return smartphoneService.delete(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
-
 }
