758bb6d5cd14c875a9784ea449b5fbea636d0caf
Artem Yurov <amyurov.dev@mail.ru>
2024-05-25 14:51:37 +0300

    add controller and service class draft

---
+++ src/main/java/com/amyurov/smartphoneecom/controller/SmartphoneController.java
@@ -0,0 +1,26 @@
+package com.amyurov.smartphoneecom.controller;
+
+import com.amyurov.smartphoneecom.service.SmartphoneService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/smartphones")
+@RequiredArgsConstructor
+@Slf4j
+public class SmartphoneController {
+
+    private static final String BASE_URI = "/api/v1/smartphones/";
+    private final SmartphoneService smartphoneService;
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        var smartphoneList = smartphoneService.findAll();
+        return ResponseEntity.ok(new ContentResponse<>(smartphoneList));
+    }
+
+}
---
+++ src/main/java/com/amyurov/smartphoneecom/service/SmartphoneService.java
@@ -0,0 +1,23 @@
+package com.amyurov.smartphoneecom.service;
+
+import com.amyurov.smartphoneecom.dto.SmartphoneReadDto;
+import com.amyurov.smartphoneecom.repository.SmartphoneRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SmartphoneService {
+
+    private final SmartphoneRepository smartphoneRepository;
+
+    public List<SmartphoneReadDto> findAll() {
+        return Collections.emptyList();
+    }
+
+}
